2cab8ebec069461b3a93cf385fd9a06542ecd785
Milagros Palmero <milagrospalmero@hotmail.com>
2022-10-27 15:31:17 -0300

    correcion en categorias

--- src/component/content/ItemListContainer/Categoria.jsx
+++ src/component/content/ItemListContainer/Categoria.jsx
@@ -1,30 +1,31 @@
 
 import {useState, useEffect} from 'react';
 import { useParams, Link } from 'react-router-dom';
-import { consultarBDD } from '../../../utils/funciones';
+
+import { getProductos } from '../../../utils/firebase';
 
 //itemListContainer
 const Categoria = () => {
     const [productos, setProductos] = useState([]);
     const {id} = useParams()
     useEffect(() => {
-       consultarBDD("../json/productos.json").then(productos => {
-        const productosCategoria = productos.filter(producto => producto.idCategoria === parseInt (id))
+       getProductos().then(productos => {
+        const productosCategoria = productos.filter(producto => producto[1].categoria == id)
         console.log(productosCategoria)
         const cardProductos = productosCategoria.map (producto =>
             
-            <div className="card cardProducto"  key={ producto.id} >
+            <div className="card cardProducto"  key={ producto[0]} >
                  <div className="container h-100">
-                <img src={producto.img} className="card-img-top img-thumbnail" alt={producto.nombre} />
+                <img src={producto[1].img} className="card-img-top img-thumbnail" alt={producto[1].nombre} />
                 
                 </div>
                 <div className="card-body">
-                     <h5 className="card-title">{producto.nombre}</h5>
+                     <h5 className="card-title">{producto[1].nombre}</h5>
                      
-                     <p className="card-text">Talles: {producto.talles} </p>
-                     <p className="card-text">Precio: ${producto.Precio} </p>
-                     <p className="card-text">Stock: {producto.stock} </p>
-                     <button   className='btn btn-dark' ><Link className='nav-link' to={"/producto/"+ producto.id}>Ver Producto</Link></button>
+                     <p className="card-text">Talles: {producto[1].talle} </p>
+                     <p className="card-text">Precio: ${producto[1].precio} </p>
+                     <p className="card-text">Stock: {producto[1].stock} </p>
+                     <button   className='btn btn-dark' ><Link className='nav-link' to={`/producto/${producto[0]}`}>Ver Producto</Link></button>
                  </div>
             </div>  ) 
     
